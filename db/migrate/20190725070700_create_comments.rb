897d12fd20fe58f7cc4146b34d790661d8c857a3
Brian Hong <hong.matchb@gmail.com>
2019-07-25 07:24:24 +0000

    Making the Submit Button Save the Comment

--- db/migrate/20190725070700_create_comments.rb
+++ db/migrate/20190725070700_create_comments.rb
@@ -8,6 +8,6 @@ class CreateComments < ActiveRecord::Migration[5.2]
     end
 
     add_index :comments, [:user_id, :place_id]
-    add_index :comment, :place_id
+    add_index :comments, :place_id
   end
 end
